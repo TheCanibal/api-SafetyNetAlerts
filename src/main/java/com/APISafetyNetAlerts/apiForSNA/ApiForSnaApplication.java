c49006390a17ea7830942bfb9b3d2a04ee083058
TheCanibal <ziryadraenei@hotmail.fr>
2023-06-24 10:03:52 +0200

    Création du projet avec Spring Boot Initializr

---
+++ src/main/java/com/APISafetyNetAlerts/apiForSNA/ApiForSnaApplication.java
@@ -0,0 +1,13 @@
+package com.APISafetyNetAlerts.apiForSNA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForSnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForSnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/APISafetyNetAlerts/apiForSNA/ApiForSnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APISafetyNetAlerts.apiForSNA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForSnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
